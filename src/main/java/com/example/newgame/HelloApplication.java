11eecef77a91c0eb2a4a4bb21f6f649ff47398c6
Lenovo <1491338835@qq.com>
2024-05-25 21:33:08 +0800

    2nd commit

--- src/main/java/com/example/newgame/HelloApplication.java
+++ src/main/java/com/example/newgame/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
         //helloApplication
         //oooo
+        //
     }
 
     public static void main(String[] args) {
